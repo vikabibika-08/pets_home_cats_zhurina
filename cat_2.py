8c08aa749d18549c94768b7fee3a205844172344
vikabibika-08 <zhurinaviktoria@yandex.ru>
2021-01-24 11:18:04 +0300

    aa

---
+++ cat_2.py
@@ -0,0 +1,11 @@
+from ourcats import OurCats
+
+cat_1 = OurCats('Baron','мальчик',2)
+print(f'Это {cat_1.name}, {cat_1.sex}. Возраст: {cat_1.age} года')
+
+
+cat_2 = OurCats('Сэм','мальчик',2)
+print(f'Это {cat_2.name}, {cat_2.sex}. Возраст: {cat_2.age} года')
+
+
+
